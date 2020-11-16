d4220a4bf45fc53be0a155edf04e609da760af40
Alejandro <alomarbar@gmail.com>
2020-11-16 03:57:18 +0100

    To be fixed: Kafka connection error

--- dockerizacion/scenario/mongodb/import_distances.sh
+++ dockerizacion/scenario/mongodb/import_distances.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Import our enriched airline data as the 'airlines' collection
-mongoimport -d agile_data_science -c origin_dest_distances --file /usr/src/mongodb/data/origin_dest_distances.jsonl
+mongoimport -d agile_data_science -c origin_dest_distances --file /usr/src/flight_prediction/origin_dest_distances.jsonl
 mongo agile_data_science --eval 'db.origin_dest_distances.ensureIndex({Origin: 1, Dest: 1})'
