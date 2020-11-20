9505a7fc9ac81c8b79367f751ec558b5a11a4d7e
dveni <28764301+dveni@users.noreply.github.com>
2020-11-20 11:15:07 +0100

    Modificacion del script buildjar para evitar fallos de no reconocimiento de comandos por culpa de saltos de linea

--- initial_configs/jar_builder/buildjar.sh
+++ initial_configs/jar_builder/buildjar.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-cd /mnt/scala_projects/${PROJECT_NAME} && rm -rf project/META-INF/ \
-&& sbt clean cleanFiles && sbt compile && sbt package
+cd /mnt/scala_projects/${PROJECT_NAME} && rm -rf project/META-INF/ && sbt clean cleanFiles && sbt compile && sbt package
